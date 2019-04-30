742b4f7f706ee4b1e25ed7b8685b352de2aef011
pengzhaopeng <17688700269@163.com>
2019-04-30 09:26:45 +0800

    1:测试

--- recommender/dataloader/src/main/scala/com/pengzhaopeng/DataLoader.scala
+++ recommender/dataloader/src/main/scala/com/pengzhaopeng/DataLoader.scala
@@ -122,7 +122,7 @@ object DataLoader {
 
     //对数据表建立索引
     mongoClient(mongoConfig.db)(MONGODB_MOVIE_COLLECTION).createIndex(MongoDBObject("mid" -> 1))
-    mongoClient(mongoConfig.db)(MONGODB_RATING_COLLECTION).
+//    mongoClient(mongoConfig.db)(MONGODB_RATING_COLLECTION).
 //      mongoClient(mongoConfig.db)(MONGODB_TAG_COLLECTION)
 //    .
 
